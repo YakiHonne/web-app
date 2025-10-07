23fdeda2ddf2fbdd6a31c484653ff857b9d1c84f
Mostafa Khaldi <mostafa.khaldi@outlook.com>
2025-10-07 12:05:01 +0800

    unsupported kind page

--- src/Components/ElementOptions/EventOptions.js
+++ src/Components/ElementOptions/EventOptions.js
@@ -396,7 +396,7 @@ export default function EventOptions({
       case "repEventsCard":
         return [
           postAsNote,
-          copyNaddr,
+          event.kind >= 30000 ? copyNaddr : copyID,
           copyPubkey,
           showRawEventContent,
           broadcastEvent,
