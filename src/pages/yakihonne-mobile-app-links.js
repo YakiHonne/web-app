24b731f163095ef39b5f073fa1caa5d8e93c3a20
Mostafa Khaldi <mostafa.khaldi@outlook.com>
2025-11-03 14:37:32 +0800

    fix fetching newest profile

--- src/pages/yakihonne-mobile-app-links.js
+++ src/pages/yakihonne-mobile-app-links.js
@@ -1,12 +1,22 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
+import HeadMetadata from '@/Components/HeadMetadata';
 
 const ClientComponent = dynamic(() => import("@/(PagesComponents)/YMARedirection"), {
   ssr: false,
 });
 
 export default function index() {
+  let data = {
+    path: "yakihonne-mobile-app-links",
+    title: "YakiHonne Mobile App",
+    description: "Download YakiHonne for iOS & iPadOS, Android (phones & tablets), and macOS.",
+    image: "https://yakihonne.s3.ap-east-1.amazonaws.com/media/images/mobile-app-download.png",
+  }
   return (
-   <ClientComponent />
-  )
+    <div>
+      <HeadMetadata data={data} />
+      <ClientComponent />
+    </div>
+   )
 }
